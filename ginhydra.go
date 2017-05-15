072269bd4b8e6cfed84dccd7ed6a63ba7e287938
Jan Olszak <janekolszak@gmail.com>
2017-05-16 00:17:13 +0200

    Parsing access_token from the query string

--- ginhydra.go
+++ ginhydra.go
@@ -3,7 +3,10 @@ package ginhydra
 import (
 	hydra "github.com/ory-am/hydra/sdk"
 
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
 )
 
 var (
@@ -14,9 +17,25 @@ func Init(hydraClient *hydra.Client) {
 	hc = hydraClient
 }
 
+// TODO: Copied from fosite
+func AccessTokenFromRequest(req *http.Request) string {
+	auth := req.Header.Get("Authorization")
+	split := strings.SplitN(auth, " ", 2)
+	if len(split) != 2 || !strings.EqualFold(split[0], "bearer") {
+		// Empty string returned if there's no such parameter
+		err := req.ParseForm()
+		if err != nil {
+			return ""
+		}
+		fmt.Println(req.Form.Get("access_token"))
+		return req.Form.Get("access_token")
+	}
+	return split[1]
+}
+
 func ScopesRequired(scopes ...string) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		ctx, err := hc.Introspection.IntrospectToken(c, hc.Warden.TokenFromRequest(c.Request), scopes...)
+		ctx, err := hc.Introspection.IntrospectToken(c, AccessTokenFromRequest(c.Request), scopes...)
 		if err != nil {
 			c.Error(err)
 			c.Abort()
